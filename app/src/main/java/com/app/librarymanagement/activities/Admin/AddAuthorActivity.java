bc9ee2456482e537780725e3e90802f93325fb3b
gopikrishna456 <111789923+gopikrishna456@users.noreply.github.com>
2023-03-03 18:32:26 -0600

    commit

--- app/src/main/java/com/app/librarymanagement/activities/Admin/AddAuthorActivity.java
+++ app/src/main/java/com/app/librarymanagement/activities/Admin/AddAuthorActivity.java
@@ -9,7 +9,8 @@ import com.app.librarymanagement.R;
 
 public class AddAuthorActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_author);
         findViewById(R.id.btnBack).setOnClickListener(view->{
